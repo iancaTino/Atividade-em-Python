53204ad62f3eab9dae7cc565d5e79a3ee1a40669
iancaTino <iancalaurentino@gmail.com>
2023-11-21 19:31:38 -0300

    Subir

--- jogoLuta.py
+++ jogoLuta.py
@@ -17,32 +17,32 @@ while vida_jogador > 0 and vida_computador > 0:
     if escolha == 1:
         poder_jogador = 4
         dano_jogador = 1
-        dano_computador = 3
+        dano_computado = 3
     elif escolha == 2:
         poder_jogador = random.randint(1, 6)
         dano_jogador = random.randint(1, 6)
-        dano_computador = 0
+        dano_computado = 0
     elif escolha == 3:
         poder_jogador = random.randint(1, 6)
         dano_jogador = 3
-        dano_computador = 3
+        dano_computado = 3
     else:
         print('Escolha inválida, tente novamente.\n1 - cabeçada\n2 - soco\n3 - chute')
         continue
 
     # Ataque do computador
     poder_computador = random.randint(1, 6)
-    dano_computador = random.randint(1, 6)
+    dano_computado = random.randint(1, 6)
 
     # Cálculo do resultado do jogo
     if poder_jogador > poder_computador:
         vida_computador -= dano_jogador
     elif poder_jogador < poder_computador:
-        vida_jogador -= dano_computador
+        vida_jogador -= dano_computado
 
     ataque_jogador = ataques[escolha]  # Obtém o nome do ataque escolhido
     print(f'Você usou o ataque {ataque_jogador} com poder {poder_jogador} e causou {dano_jogador} de dano.')
-    print(f'O computador usou o ataque com poder {poder_computador} e causou {dano_computador} de dano.')
+    print(f'O computador usou o ataque com poder {poder_computador} e causou {dano_computado} de dano.')
     print(f'Seu total de vida: {vida_jogador}')
     print(f'Vida do computador: {vida_computador}')
 
